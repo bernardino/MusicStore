bac9e0c394ceffd2228f3d380269a4c113e9c9c2
bernardino <ricardo.bernardino@gmail.com>
2010-12-19 02:06:06 +0000

    designs iguais

--- store.rb
+++ store.rb
@@ -7,17 +7,31 @@ require 'oci8'
 require 'net/http'
 require 'uri'
 require 'rexml/document'
-require 'cenas'
+require './cenas'
 
 get '/' do
   erb :index
 end
 
 get '/artist/:id' do
-	#@artistID = params[:id]
-	db.select("SELECT artist_id from artist") do |r|
-		@artistID = r.last
+	@artistID = params[:id]
+	#db.select("SELECT artist_id from artist") do |r|
+	#	@artistID = r.last
+	#end
+	url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=#{params[:id]}&api_key=b25b959554ed76058ac220b7b2e0a026" #LAST.FM REST API
+	resp = Net::HTTP.get_response(URI.parse(url)).body
+	
+	doc = REXML::Document.new resp
+	doc.elements.each("lfm/artist/image") do |r|
+		if r.attributes["size"] == 'large'
+			@image = r.text
+		end
 	end
+	
+	doc.elements.each("lfm/artist/bio/summary") do |r|
+		@bio = r.text
+	end
+	
   erb :artist
 end
 
@@ -35,6 +49,27 @@ get '/song/:id' do
 		
 	end
   erb :song
+end
+
+get '/album/:id' do
+  @albumID = params[:id]
+  
+  url = 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=b25b959554ed76058ac220b7b2e0a026&artist=Cher&album=Believe' #LAST.FM REST API
+	resp = Net::HTTP.get_response(URI.parse(url)).body
+	
+	doc = REXML::Document.new resp
+	doc.elements.each("lfm/album/image") do |r|
+		if r.attributes["size"] == 'large'
+			@image = r.text
+		end
+		
+	end
+  
+  erb :album
+end
+
+get '/search' do
+  erb :search
 end
 
 get '/merch/:id' do
