e8a57fe9659c7850479584ad1135a219ce954470
bernardino <ricardo.bernardino@gmail.com>
2010-12-22 21:39:35 +0000

    sessions added

--- store.rb
+++ store.rb
@@ -15,13 +15,60 @@ configure do
 	$db = Database.new
 	$lf = Lastfm.new
 	#$lf.update_artist(params[:id])
+	enable :sessions
+end
+
+before do
+  if session[:id]
+    @logged = true
+  else
+    @logged = false
+  end
 end
 
 get '/' do
   erb :index
 end
 
+get '/register' do
+  if session[:id]
+    redirect '/'
+  end
+  @message=params[:message]
+  if @message
+    @message="Username already in use!"
+  end
+  erb :register
+end
+
+post '/register' do
+  res = $db.select("select client_id from client where upper(client_id) like upper('#{params[:username]}')")
+  unless res[0]
+    $db.execute("insert into client values('#{params[:username]}','#{params[:address]}',#{params[:phone]}, '#{params[:name]}', '#{params[:passcode]}', '#{params[:email]}')")
+    $db.execute("commit")
+    session[:id] = params[:username]
+    redirect '/'
+  else
+    redirect '/register?message=error'
+  end
+end
+
+post '/login' do
+  res = $db.select("select client_id from client where upper(client_id) like upper('#{params[:username]}') and password like '#{params[:password]}'")
+  if res[0]
+    session[:id] = params[:username]
+  end
+  
+  redirect '/'
+end
+
+get '/logout' do
+  session[:id] = nil
+  redirect '/'
+end
+
 get '/artist/:id' do
+  $lf.update_artist(String.new(params[:id]))
 	res = $db.select("SELECT artist_name,artist_bio,artist_image FROM artist
 						WHERE upper(artist_name) like upper('#{params[:id]}')")
 	@artistID = String.new(params[:id])
