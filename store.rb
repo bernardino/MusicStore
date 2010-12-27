26c794c7a92f253febb3deebfb99b1b2dabe560c
bernardino <ricardo.bernardino@gmail.com>
2010-12-27 17:29:58 +0000

    checkout added

--- store.rb
+++ store.rb
@@ -164,9 +164,25 @@ end
 
 get '/addorder' do
   if session[:orders][params[:id]]
-    session[:orders][params[:id]] = session[:orders][params[:id]]+1
+    session[:orders][params[:id]][0] += 1
   else
-    session[:orders][params[:id]]=1
+    session[:orders][params[:id]] = [] #[0]->quantity, [1]->name, [2]->type
+    if params[:type]=='a' #album
+      res = $db.select("select album_name from album where product_id = '#{params[:id]}'")
+      session[:orders][params[:id]] << 1
+      session[:orders][params[:id]] << res[0]
+      session[:orders][params[:id]] << params[:type]
+    elsif params[:type]=='m' #merchandise
+      res = $db.select("select merchandise_name from album where product_id = '#{params[:id]}'")
+      session[:orders][params[:id]] << 1
+      session[:orders][params[:id]] << res[0]
+      session[:orders][params[:id]] << params[:type]
+    elsif params[:type]=='s' #song
+      res = $db.select("select song_name from album where product_id = '#{params[:id]}'")
+      session[:orders][params[:id]] << 1
+      session[:orders][params[:id]] << res[0]
+      session[:orders][params[:id]] << params[:type]
+    end
   end 
   redirect params[:page]
 end
@@ -175,7 +191,17 @@ get '/checkout' do
   unless session[:id]
     redirect '/'
   end
-  erb :cenas
+  
+  erb :checkout
+end
+
+get '/removeorder' do
+  session[:orders].delete(params[:id])
+  redirect params[:page]
+end
+
+get 'final' do
+  redirect '/'
 end
 
 get '/addvote' do
